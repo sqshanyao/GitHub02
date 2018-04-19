42933c45cce052a9ae976fe0f65ff7b610c34348
shanyao <sqshanyao@163.com>
2018-04-19 15:52:20 +0800

    md测试

--- sourcecodestudy/src/com/yao/sourcecode/jdk/map/ConCurrentHashMapStudy.java
+++ sourcecodestudy/src/com/yao/sourcecode/jdk/map/ConCurrentHashMapStudy.java
@@ -2,12 +2,17 @@ package com.yao.sourcecode.jdk.map;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * Created by user on 2018/4/19.
  */
 public class ConCurrentHashMapStudy {
     @Test
     public void testConcurrentHashMap() {
-        
+        ConcurrentHashMap map = new ConcurrentHashMap();
+        map.put("","");
+        HashMap hashMap = new HashMap();
     }
 }
